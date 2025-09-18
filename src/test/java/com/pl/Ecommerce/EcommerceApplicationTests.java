cdea3c5b18a6f9c3d7a3f05806af347a07ccc6a1
PawelPloszaj <41387367+PawelPloszaj@users.noreply.github.com>
2025-09-18 16:59:18 +0200

    chore: initialize project with Spring Boot

---
+++ src/test/java/com/pl/Ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
